d3630d0f057e60cdaa5e2554818507b323634341
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-11-12 18:05:48 -0500

    Add quotes array

--- js/script.js
+++ js/script.js
@@ -7,10 +7,32 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
-
+// An array of objects to store the quotes
+const quotes = [
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    source: "Walt Disney"
+  },
+  {
+    quote: "I've failed over and over and over again in my life. And that is why I succeed.",
+    source: "Michael Jordan",
+    citation: "Nike commercial"
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life.",
+    source: "Steve Jobs",
+    citation: "Stanford Commencement Address",
+    year: 2005
+  },
+  {
+    quote: "It always seems impossible until it's done.",
+    source: "Nelson Mandela"
+  },
+  {
+    quote: "If you really want something, and really work hard, and take advantage of opportunities, and never give up, you will find a way.",
+    source: "Jane Goodall"
+  }
+];
 
 
 /***
